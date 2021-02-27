0f0696528e1ee3f79b1352ff14e58a021d5d12d2
lochungtin <lochungtin@gmail.com>
2021-02-28 02:09:27 +0800

    project categories

--- src/sections/projects.js
+++ src/sections/projects.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import '../css/web/projects.css';
 import { tags } from '../data/tags';
 
 class Screen extends React.Component {
 
-    style = styleName => `project-${(this.props.mobile ? 'm-' : '') + styleName}`
+    style = styleName => `project-${(this.props.mobile ? 'm-' : '') + styleName}`;
 
     render() {
+        console.log(this.style('filter-category-container'))
+
         return (
             <div className={'row ' + this.style('root')}>
                 <div className={this.style('filter')}>
                     {Object.keys(tags).map(category => {
                         return (
-                            <div className={this.style('filter-category-container')}>
+                            <div className={this.style('filter-category-container')} key={category}>
                                 <p className={this.style('filter-category')}>
                                     {category}
                                 </p>
+                                
                                 <div>
                                     {tags[category].map(tag => {
                                         return (
-                                            <p className={this.style('filter-tag')}>
+                                            <p className={this.style('filter-tag')} key={tag.name}>
                                                 {tag.display}
                                             </p>
                                         );
